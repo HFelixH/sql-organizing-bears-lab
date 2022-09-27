01c9ba49d6f9d1f34bc0eab45ae556cc2eb7bae9
HFelixH <felix-hoang@outlook.com>
2022-09-27 12:17:45 +0930

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, sex, color) 
+VALUES ('Mr. Chocolate', 'M', 'White'),
+
+ ('Rowdy', 'F', 'White'),
+
+ ('Tabitha', 'F', 'Pink'),
+
+ ('Sergeant Brown', 'M', 'Green'),
+
+ ('Melissa', 'F', 'Yellow'),
+
+ ('Grinch', 'M', 'Brown'),
+
+ ('Wendy', 'M', 'Red'),
+
+ (NULL, 'M', 'Black');
